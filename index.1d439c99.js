a9a8cc8d720a8e3b2168f9844f2f401e47b6e907
ViktorSvertoka <victor.svertoka@gmail.com>
2023-01-09 19:03:58 +0000

    Deploying to gh-pages from @ ViktorSvertoka/ice-cream@917e579b87e30a44cfbde8120488b6e4d0438cc9 🚀

--- index.1d439c99.js
+++
@@ -1,2 +0,0 @@
-let e=0;!function l(){let t,s=document.getElementsByClassName("mySlides");for(t=0;t<s.length;t++)s[t].style.display="none";e++,e>s.length&&(e=1);s[e-1].style.display="block",setTimeout(l,3e3)}();
-//# sourceMappingURL=index.1d439c99.js.map
--- index.60d01a82.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=0;!function t(){var l,n=document.getElementsByClassName("mySlides");for(l=0;l<n.length;l++)n[l].style.display="none";++e>n.length&&(e=1);n[e-1].style.display="block",setTimeout(t,3e3)}()}();
-//# sourceMappingURL=index.60d01a82.js.map
